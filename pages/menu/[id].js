74b5a1dc418437ad7946d971b052884ab9be27c2
Knight <geetaabhinav2001@gmail.com>
2021-11-14 18:33:36 +0530

    minor update

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -66,7 +66,7 @@ const Menu = () => {
                     className="bg-green active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => {
-                      if (totalPrice < 400) {
+                      if (totalPrice < hotel?.minOrder) {
                         return;
                       }
                       let orderArray = [];
